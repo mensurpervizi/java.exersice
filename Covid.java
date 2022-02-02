93dc8a90c5df9ed151b8c1ef3006e546f20a0735
mensurpervizi <m.pervizi@albansi.com>
2022-02-02 15:52:10 -0800

    exersices

---
+++ Covid.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Covid {
+    public static void main(String[] args) {
+        int[] num;
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("enter a number in a range of 3 ");
+        num=new int[2];
+        for(int i-0;i<3;i++){
+            int a=scanner.nextInt();
+            if(a>3 || a<0);
+            i--;
+           } else{
+            num[i]=a;
+        }
+
+        
+    }
+
+}
